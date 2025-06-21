247470d6ffc85a1e19827b591b2b0ad875cacd1f
dhruvyellanki19 <dhruvyellanki19>
2025-06-21 18:37:28 +0000

    pushing spark_setup.py

---
+++ scripts/spark_setup.py
@@ -0,0 +1,9 @@
+from pyspark.sql import SparkSession
+
+def get_spark_session(app_name="credit_risk_prediction_pipeline"):
+    spark = SparkSession.builder \
+        .appName(app_name) \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .getOrCreate()
+    return spark
